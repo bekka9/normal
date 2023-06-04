b6ccbeb7a9027380fa05c613529fc43bd51a20b0
bekka9 <57871070+bekka9@users.noreply.github.com>
2023-06-05 01:28:00 +0300

    Update sur_n.m

--- sur_n.m
+++ sur_n.m
@@ -1,28 +1,25 @@
-clear;
-x = -pi:0.1:pi;
-y = -pi:0.1:pi;
-[X, Y] = meshgrid(x, y);
-Z = 20 - X.^2 - Y.^2;
-surf(X, Y, Z);
-shading interp;
+function normal(f, x0, y0)
+  x = -pi:0.1:pi;
+  y = -pi:0.1:pi;
+  [X, Y] = meshgrid(x, y);
+  Z = f(X,Y);
+  surf(X, Y, Z);
+  shading interp;
+  hold on;
 
-hold on;
+  z0 = f(x0, y0);
 
-f = @(x, y) 20 - x.^2 - y.^2;
+  h = 0.000001;
+  f_dx = (f(x0+h, y0) - f(x0, y0)) / h;
+  f_dy = (f(x0, y0+h) - f(x0, y0)) / h;
 
-x0 = 0;
-y0 = -1;
-z0 = f(x0, y0);
+  n_ort = [-f_dx -f_dy 1] / sqrt((f_dx)^2 + (f_dy)^2 + 1);
 
-h = 0.000001;
-f_der_x = (f(x0+h, y0) - f(x0, y0)) / h;
-f_der_y = (f(x0, y0+h) - f(x0, y0)) / h;
+  x_norm = [x0 x0 + n_ort(1)*20];
+  y_norm = [y0 y0 + n_ort(2)*20];
+  z_norm = [z0 z0 + n_ort(3)*20];
 
-n_ort = [-f_der_x -f_der_y 1] / sqrt((f_der_x)^2 + (f_der_y)^2 + 1);
-
-x_norm = [x0 x0+n_ort(1)];
-y_norm = [y0 y0+n_ort(2)];
-z_norm = [z0 z0+n_ort(3)];
-
-plot3(x_norm, y_norm, z_norm);
-axis equal;
+  plot3(x_norm, y_norm, z_norm);
+  axis equal;
+  hold off;
+end
